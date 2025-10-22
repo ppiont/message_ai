183a1a20130a0189b6546155a643431190d878d0
Peter Piontek <piontek0@gmail.com>
2025-10-22 14:04:22 -0700

    fix: upgrade desugar_jdk_libs to required version
    Changed from 2.0.4 to 2.1.4 as required by flutter_local_notifications.
    
    Error fixed:
      Dependency ':flutter_local_notifications' requires desugar_jdk_libs version
      to be 2.1.4 or above for :app, which is currently 2.0.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -63,5 +63,5 @@ flutter {
 
 dependencies {
     // Core library desugaring (required for flutter_local_notifications)
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
